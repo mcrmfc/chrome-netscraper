8105b48d384b1d3b2d803e8fdca7de61a6fd33fd
mcrmfc <matthew.robbins@itv.com>
2013-12-04 15:29:26 +0000

    expose extension id to page

--- netscraper/timer_content.js
+++ netscraper/timer_content.js
@@ -1,6 +1,7 @@
 chrome.storage.onChanged.addListener(function(changes) {
     chrome.storage.local.get("filter", function(obj) {
         var filter = obj['filter'];
+        var extensionId = chrome.i18n.getMessage("@@extension_id");
         var requestURL = changes[window.location.href]['newValue'];
         var parser = document.createElement('a');
         parser.href = requestURL;
@@ -11,12 +12,12 @@ chrome.storage.onChanged.addListener(function(changes) {
                 detail: {
                     message: requestURL,
                     time: new Date(),
+                    extensionId: extensionId
                 },
                 bubbles: true,
                 cancelable: true
             }
         );
-        console.log(filter);
         if (!filter || (host == filter)) {
             document.getElementsByTagName("body")[0].dispatchEvent(event);
         }
