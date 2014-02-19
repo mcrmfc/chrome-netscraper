83227231e9a353972dfdf43cf22cfbc16ba72bd1
Matthew Robbins <matthew.robbins@itv.com>
2014-02-19 15:44:57 +0000

    updates to grab post

--- netscraper/background.js
+++ netscraper/background.js
@@ -1,13 +1,33 @@
+var counter = 0;
 function logNetRequest(details)  {
-    var urlRequested = details.url;
-    var callingTab = details.tabId;
-    var callingTabUrl;
-    chrome.tabs.get(callingTab, function(tab) { callingTabUrl = tab.url; report(callingTabUrl, urlRequested);});
+    var callingTab = details.tabId,
+    arrayBufferString = '',
+    callingTabUrl;
+
+    var requestDetails = {};
+    requestDetails.url = details.url;
+    requestDetails.method = "GET";
+
+    //Get request body if it exists - could do with some code love here
+    if(details.requestBody) {
+        if(details.requestBody.raw) {
+            if(details.requestBody.raw[0]) {
+                requestDetails.method = "POST";
+                var arrayBuffer = details.requestBody.raw[0].bytes;
+                arrayBufferString = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer));
+                requestDetails.requestBody = arrayBufferString;
+            }
+        }
+    }
+
+    if(callingTab >= 0) {
+        chrome.tabs.get(callingTab, function(tab) { callingTabUrl = tab.url; report(callingTabUrl, requestDetails);});
+    }
 }
 
-function report(callingTabUrl, urlRequested) {
+function report(callingTabUrl, requestDetails, requestBody) {
     var obj = {}
-    obj[callingTabUrl] = urlRequested;
+    obj[callingTabUrl] = JSON.stringify(requestDetails);
     chrome.storage.local.set(obj);
 }
 
